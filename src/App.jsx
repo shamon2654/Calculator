a533c31fe7c8f93bbf36f5e36d2f48d7e280cc46
shamon2654 <shamons226@gmail.com>
2023-12-21 21:03:54 +0530

    feat:error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
+import { useState } from "react"
 import "./App.css"
 import Calculator from "./Component/Calculator"
 
-
 function App() {
+  const [value, setValue] = useState("")
+  const [result, setResult] = useState("")
   return (
     <>
-      <Calculator />
+      <Calculator/>
     </>
   )
 }
